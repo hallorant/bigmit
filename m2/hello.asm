81b09c7d74ce0cebef573d9f9dc66e637570129b
Tim Halloran <hallorant@gmail.com>
2020-05-03 19:37:19 -0400

    Style changes for m2 code.

--- m2/hello.asm
+++ m2/hello.asm
@@ -1,21 +1,22 @@
 ; Toy Model 2 program that writes "Hello TRS-80 Model II" at the center
 ; of the screen via operating system calls.
-		org $3000
-text		defb	'Hello TRS-80 Model II'	
+  org $3000
+text	defb	'Hello TRS-80 Model II'	
 main:
 ; Use VDINIT (pg 234) to clear the screen.
-		ld b,1
-		ld c,1
-		ld a,7		; TRSDOS Function code
-		rst 8
+  ld b,1
+  ld c,1
+  ld a,7   ; TRSDOS function code
+  rst 8
 ; Use VDGRAF (pg 237) to display our text.
-		ld b,11		; row
-		ld c,29		; column
-		ld d,21		; 'text' length
-		ld hl,text
-		ld a,10
-		rst 8
+  ld b,11  ; row
+  ld c,29  ; column
+  ld d,21  ; 'text' length
+  ld hl,text
+  ld a,10  ; TRSDOS function code
+  rst 8
 ; Just hang the machine here so we can see the text.
 ; This is bad form on an OS.
-hcf:		jp hcf
-		end main
+hcf:
+  jp hcf
+  end main
