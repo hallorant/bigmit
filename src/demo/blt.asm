1c4b1a74275f3074745784e97d41c9d929761d29
Tim Halloran <hallorant@gmail.com>
2020-05-17 13:41:21 -0400

    blt mostly works, some odd Model 3 line?

--- src/demo/blt.asm
+++ src/demo/blt.asm
@@ -3,16 +3,25 @@
 import '../lib/barden_fill.asm'
 import '../lib/gp_14byte_move.asm'
 
+stack_space	defs	400
+stack		equ	$-1
+
 screen		equ	$3c00
 row		equ	64
+
 ; Position the raycasting window on the screen.
 window		equ	screen+64+3
 window_width	equ	27
 window_height	equ	9
 bb_width	equ	window_width
 bb_height	equ	window_height
-gb_width	equ	window_width
-gb_height	equ	window_height*3
+gb_width	equ	bb_width
+gb_height	equ	bb_height*3
+
+; This back buffer represnts a square on the TRS-80 screen.
+; then this buffer is transferred onto the TRS-80 screen.
+bb		defs	bb_width*bb_height
+bb_size		equ	$-bb
 
 ; This graphics buffer (gb) layout differs from the back buffer (bb) and screen layout.
 ; It defines "pixels", one per byte, which are 2x1 TRS-80 graphics blocks. This gives us
@@ -20,22 +29,79 @@ gb_height	equ	window_height*3
 ; Each row of in the gb represents a column in the bb. Defining bytes, left to right, that
 ; fill in 2x1 screen pixels (three per screen row) from top to bottom of the bb.
 gb		defs	gb_width*gb_height
-gb_end		equ	$
+gb_size		equ	$-gb
+
+img32  		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$ff,$ff,$ff,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $ff,$ff,$ff,$00,$00,$00,$00,$00,$ff,$ff, $ff,$ff,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$ff,$ff,$ff,$00,$00
+   		defb	$00,$00,$00,$00,$00,$ff,$ff,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$ff,$ff
+   		defb	$00,$00,$ff,$ff,$ff,$00,$ff,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00
+   		defb	$ff,$ff,$00,$00,$00,$ff,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$ff,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$ff,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$ff,$ff,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$ff,$ff,$00,$00,$00,$00, $ff,$ff,$ff,$ff,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$ff,$ff,$00,$ff, $00,$00,$00,$00,$ff,$ff,$ff
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$ff,$ff,$ff, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$ff,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$ff,$ff,$ff, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$ff,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$ff,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$ff,$ff,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$ff,$ff,$ff,$ff,$00,$00,$00,$ff,$00,$00, $00,$ff,$ff,$00,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$ff,$00,$ff,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$ff,$ff,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$ff,$ff,$ff
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $ff,$ff,$ff,$00,$00,$00,$00,$00,$00,$00, $ff,$ff,$ff,$ff,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$ff,$ff,$ff,$ff,$ff,$ff, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+
+
+img_col		defb	0
 
 ; Scroll the graphics buffer to the right one pixel.
-scroll_gb macro
-  ld hl,gb_end-gb_width
-  ld de,gb_end
-  ld bc,gb_width*gb_height-gb_width
+scroll_gb_right_one_pixel macro
+  ld hl,gb+gb_size-1-gb_width
+  ld de,gb+gb_size-1
+  ld bc,gb_size-gb_width
   lddr
   endm
 
-; This back buffer represnts a square on the TRS-80 screen.
-; then this buffer is transferred onto the TRS-80 screen.
-bb		defs	bb_width*bb_height
+animate_img_to_gb macro
+  scroll_gb_right_one_pixel
+  ld hl,img32
+  ; Deterine which column of the image to add
+  ld c,$1f ; 0001_1111 (mask)
+  ld a,(img_col)
+  inc a
+  and c
+  ld (img_col),a
+  or a ; is a == 0?
+  jr z,_img_col_draw
 
-; hl must point to the positon in the graphics buffer.
-draw_bb_pos macro bb_dst
+  ld b,a ; columns over
+  ld de,gb_width
+_img_ptr_loop:
+  add hl,de
+  djnz _img_ptr_loop
+_img_col_draw:
+  ld de,gb
+  ld bc,gb_width
+  ldir
+  endm
+
+; hl must point to the correct position in the graphics buffer.
+draw_gb_to_bb_char macro bb_dst
   ld d,$80 ; empty graphics character
   ld a,$83 ; top 2x1 dot
   and (hl)
@@ -51,96 +117,165 @@ draw_bb_pos macro bb_dst
   and (hl)
   or d
   ld (bb_dst),a ; write character result to dst (in back buffer)
+  inc hl
   endm
 
-draw_bb_vert macro gp_src,bb_dst
-  ld hl,gp_src
-  draw_bb_pos bb_dst+bb_width*0
-  draw_bb_pos bb_dst+bb_width*1
-  draw_bb_pos bb_dst+bb_width*2
-  draw_bb_pos bb_dst+bb_width*3
-  draw_bb_pos bb_dst+bb_width*4
-  draw_bb_pos bb_dst+bb_width*5
-  draw_bb_pos bb_dst+bb_width*6
-  draw_bb_pos bb_dst+bb_width*7
-  draw_bb_pos bb_dst+bb_width*8
+; blts a single vertical column on the screen (9 lines).
+; hl must point to the start row position in the graphics buffer.
+; 9 rows of the screen (hardcoded)
+draw_gb_to_bb_column macro bb_dst
+  draw_gb_to_bb_char bb_dst+bb_width*0
+  draw_gb_to_bb_char bb_dst+bb_width*1
+  draw_gb_to_bb_char bb_dst+bb_width*2
+  draw_gb_to_bb_char bb_dst+bb_width*3
+  draw_gb_to_bb_char bb_dst+bb_width*4
+  draw_gb_to_bb_char bb_dst+bb_width*5
+  draw_gb_to_bb_char bb_dst+bb_width*6
+  draw_gb_to_bb_char bb_dst+bb_width*7
+  draw_gb_to_bb_char bb_dst+bb_width*8
   endm
 
-gb_to_bb macro
-  draw_bb_vert gb+gb_height*0,bb+bb_width*0
-  draw_bb_vert gb+gb_height*1,bb+bb_width*1
-  draw_bb_vert gb+gb_height*2,bb+bb_width*2
-  draw_bb_vert gb+gb_height*3,bb+bb_width*3
-  draw_bb_vert gb+gb_height*4,bb+bb_width*4
-  draw_bb_vert gb+gb_height*5,bb+bb_width*5
-  draw_bb_vert gb+gb_height*6,bb+bb_width*6
-  draw_bb_vert gb+gb_height*7,bb+bb_width*7
-  draw_bb_vert gb+gb_height*8,bb+bb_width*8
-  draw_bb_vert gb+gb_height*9,bb+bb_width*9
-  draw_bb_vert gb+gb_height*10,bb+bb_width*10
-  draw_bb_vert gb+gb_height*11,bb+bb_width*11
-  draw_bb_vert gb+gb_height*12,bb+bb_width*12
-  draw_bb_vert gb+gb_height*13,bb+bb_width*13
-  draw_bb_vert gb+gb_height*14,bb+bb_width*14
-  draw_bb_vert gb+gb_height*15,bb+bb_width*15
-  draw_bb_vert gb+gb_height*16,bb+bb_width*16
-  draw_bb_vert gb+gb_height*17,bb+bb_width*17
-  draw_bb_vert gb+gb_height*18,bb+bb_width*18
-  draw_bb_vert gb+gb_height*19,bb+bb_width*19
-  draw_bb_vert gb+gb_height*20,bb+bb_width*20
+; 21 columns of the screen (hardcoded) we blt each row out of gb.
+draw_gb_to_bb macro
+  ld hl,gb
+  draw_gb_to_bb_column bb+0
+  draw_gb_to_bb_column bb+1
+  draw_gb_to_bb_column bb+2
+  draw_gb_to_bb_column bb+3
+  draw_gb_to_bb_column bb+4
+  draw_gb_to_bb_column bb+5
+  draw_gb_to_bb_column bb+6
+  draw_gb_to_bb_column bb+7
+  draw_gb_to_bb_column bb+8
+  draw_gb_to_bb_column bb+9
+  draw_gb_to_bb_column bb+10
+  draw_gb_to_bb_column bb+11
+  draw_gb_to_bb_column bb+12
+  draw_gb_to_bb_column bb+13
+  draw_gb_to_bb_column bb+14
+  draw_gb_to_bb_column bb+15
+  draw_gb_to_bb_column bb+16
+  draw_gb_to_bb_column bb+17
+  draw_gb_to_bb_column bb+18
+  draw_gb_to_bb_column bb+19
+  draw_gb_to_bb_column bb+20
+  draw_gb_to_bb_column bb+21
+  draw_gb_to_bb_column bb+22
+  draw_gb_to_bb_column bb+23
+  draw_gb_to_bb_column bb+24
+  draw_gb_to_bb_column bb+25
+  draw_gb_to_bb_column bb+26
   endm
 
-img32  		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$ff,$ff,$ff,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $ff,$ff,$ff,$00,$00,$00,$00,$00,$ff,$ff, $ff,$ff,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$ff,$ff,$ff,$00,$00
-   		defb	$00,$00,$00,$00,$00,$ff,$ff,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$00,$ff,$ff
-   		defb	$00,$00,$ff,$ff,$ff,$00,$ff,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$ff,$ff,$00,$00,$00,$ff,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$ff,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$ff,$00,$00,$00, $ff,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$ff,$00,$00,$00,$00,$00,$00,$ff,$00, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$ff,$ff,$00,$00,$00,$00,$ff,$00, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$ff,$ff,$00,$00,$ff,$ff, $ff,$ff,$ff,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$ff,$ff,$00,$00, $00,$ff,$00,$00,$ff,$ff,$ff
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$ff,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$00,$ff,$ff,$ff,$00, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$ff,$00,$ff,$ff,$00, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$ff,$ff,$ff,$00,$ff,$00,$00,$ff, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$00,$00,$ff,$00,$00,$00,$ff, $00,$00,$ff,$00,$00,$00,$00
-   		defb	$ff,$ff,$00,$00,$00,$00,$00,$ff,$00,$00, $00,$00,$00,$ff,$ff,$00,$00,$00,$00,$ff, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$00,$00,$ff,$ff,$00,$00,$00,$ff,$00,$00, $00,$ff,$ff,$00,$00,$00,$00,$00,$00,$ff, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$ff,$00,$00,$ff,$00,$00, $ff,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$ff,$ff,$00,$00,$ff, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$ff,$ff,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$ff,$ff,$ff
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$ff, $ff,$ff,$ff,$00,$00,$00,$00,$00,$00,$00, $ff,$ff,$ff,$ff,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$ff,$ff,$ff,$ff,$ff,$ff,$ff, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
-   		defb	$00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00,$00,$00,$00, $00,$00,$00,$00,$00,$00,$00
+draw_bb_row_to_screen macro bb_src,screen_dst
+  gp_14byte_move bb_src, screen_dst
+  ; We re-move the middle byte to use fast 14 byte moves.
+  gp_14byte_move bb_src+13, screen_dst+13
+  endm
 
-row_to_screen_27_bytes macro src,dst
-  gp_14byte_move src, dst
-  gp_14byte_move src+13, dst+13
+; 27 columns and 9 rows (hardcoded)
+draw_bb_to_screen macro
+  ld (save_sp),sp
+  draw_bb_row_to_screen bb+27*0,window+row*0
+  draw_bb_row_to_screen bb+27*1,window+row*1
+  draw_bb_row_to_screen bb+27*2,window+row*2
+  draw_bb_row_to_screen bb+27*3,window+row*3
+  draw_bb_row_to_screen bb+27*4,window+row*4
+  draw_bb_row_to_screen bb+27*5,window+row*5
+  draw_bb_row_to_screen bb+27*6,window+row*6
+  draw_bb_row_to_screen bb+27*7,window+row*7
+  draw_bb_row_to_screen bb+27*8,window+row*8
+  ld sp,(save_sp)
   endm
 
 tick	defb	0
 save_sp	defw	0
 
-; Timer interrupt handler
+ifdef model1 ; Model 1 - poll VBLANK (if mod) or just inc tick.
+
+m1_vblank	defb	1 ; Has VBLANK mod? 1 = yes, 0 = no.
+
+setup_ticks macro
+  ; TODO CHECK FOR VBLANK AND SET m1_vblank
+  nop
+  endm
+
+wait_for_next_tick macro
+  ; increment tick
+  ld a,(tick)
+  inc a
+  ld (tick),a
+
+  ; Check if we are suppose to use the VBLANK hardware.
+  ld a,(m1_vblank)
+  or a ; is a == 0?
+  jr z,_wait_is_over
+
+  ; ** Model 1 with VBLANK mod only **
+  ; Wait for the start of VBLANK. We want to see a 0 value to
+  ; Ensure we aren't jumping in at the end of VBLANK.
+_wait_in_vblank:
+  in a,($ff)
+  bit 0,a
+  jr nz,_wait_in_vblank
+_wait_not_in_vblank:
+  in a,($ff)
+  bit 0,a
+  jr z, _wait_not_in_vblank
+  ; VBLANK is beginning when we fall through to here.
+_wait_is_over
+  ld a,(tick)
+  endm
+
+else ; Model 3 or 4 - Use timer interrupt for ticks
+
+; Our timer interrupt handler.
 ; Return back to ROM interrupt processing;
-; the vector was set by init code above.
+; the vector was set by init code below.
 tick_interrupt:
   ld a,(tick)
   inc a
   ld (tick),a
-  tick_jp: jp 0 ; The 0 is self-modified into the original address.
+tick_jp:
+  jp 0 ; The 0 is self-modified into the original handler address.
+
+; Setup timer interrupt Model 3 & 4.
+setup_ticks macro
+  di
+  ld hl,(0x4013)
+  ld (tick_jp+1),hl
+  ld hl,tick_interrupt
+  ld (0x4013),hl
+  ei
+  endm
+
+; Exit: a set to (tick)
+wait_for_next_tick macro,?wait_loop
+  ld a,(tick)
+  ld b,a
+?wait_loop:
+  ld a,(tick)
+  xor b ; is a == b?
+  jr z,?wait_loop
+  endm
+
+endif
 
 main:
+  ld sp,stack
+
+  ; Clear the graphics buffer
+  ld d,$00
+  ld hl,gb
+  ld bc,gb_size
+  call barden_fill
+
+  ; Clear the back buffer
+  ld d,$80
+  ld hl,bb
+  ld bc,bb_size
+  call barden_fill
+
   ; Clear the screen
   ld d,$80
   ld hl,screen
@@ -171,50 +306,17 @@ _vl_loop:
   add iy,de
   djnz _vl_loop
   
-  ; TODO don't need this long term -- clears back buffer
-  ld d,$80
-  ld hl,bb
-  ld bc,bb_width*bb_height
-  call barden_fill
+  setup_ticks
   
-  ; Setup timer interrupt Model 3 & 4.
-  ; TODO move this into a call and support Model 1 VBLANK mod.
-  di
-  ld hl,(0x4013)
-  ld (tick_jp+1),hl
-  ld hl,tick_interrupt
-  ld (0x4013),hl
-  ei
-
-; ----- GAME LOOP -----
+  ; ---------------------
+  ; ----- GAME LOOP -----
+  ; ---------------------
 _game_loop:
+  animate_img_to_gb
+  draw_gb_to_bb
 
-  ; Ready to draw the back buffer - wait for a tick.
-  ld a,(tick)
-  ld b,a
-_wait_for_next_tick:
-  ld a,(tick)
-  xor b ; is a == b?
-  jr z,_wait_for_next_tick
-
-  ld (bb+3),a ; so we see something changing
-
-  gb_to_bb
-
-_bb_to_screen:
-  ; Hard coded to 27 columns and 9 rows
-  ld (save_sp),sp
-  row_to_screen_27_bytes bb+27*0,window+row*0
-  row_to_screen_27_bytes bb+27*1,window+row*1
-  row_to_screen_27_bytes bb+27*2,window+row*2
-  row_to_screen_27_bytes bb+27*3,window+row*3
-  row_to_screen_27_bytes bb+27*4,window+row*4
-  row_to_screen_27_bytes bb+27*5,window+row*5
-  row_to_screen_27_bytes bb+27*6,window+row*6
-  row_to_screen_27_bytes bb+27*7,window+row*7
-  row_to_screen_27_bytes bb+27*8,window+row*8
-  ld sp,(save_sp)
+  wait_for_next_tick
+  draw_bb_to_screen
 
   jp _game_loop
   end main
-
