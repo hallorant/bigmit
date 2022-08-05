38cf9fa9b0b16ff946ab017068bd9bffd0ed043a
Tim Halloran <hallorant@gmail.com>
2022-08-05 18:12:40 -0400

    Implemented both needed wordle functions with tests, but not using rest.txt yet.

--- etc/trs8bit2022_contest/dictionary.asm
+++ etc/trs8bit2022_contest/dictionary.asm
@@ -23,19 +23,79 @@ dict_start:
   include bitstream.asm
   include huffman.asm
 
-assumed_first_letter byte 'A'
-counter              word 0
-buffer_ptr           word 0
-
+; -----------------------------------------------------------------
 ; getpuz - put 5 letter answer number DE to memory at HL
-
+; -----------------------------------------------------------------
 getpuz:
-  ld (buffer_ptr),hl
-  ld (counter),de
+  jr _getpuz_start
+getpuz_answer_ptr word 0  ; passed value
+getpuz_counter    word 0  ; counts down from passed answer # to 0
+_getpuz_start:
+  ld (getpuz_answer_ptr),hl
+  ld (getpuz_counter),de
   bitstream_reset answer_data
   ; Decode a 5 letter word and store in result memory.
-_decode_answer_word:
-  ld ix,(buffer_ptr)
+_decode_all_answer_words:
+  ld ix,(getpuz_answer_ptr)
+  call decode_answer_word
+  ; Decrement the counter.
+  ld hl,(getpuz_counter)
+  dec hl
+  ld (getpuz_counter),hl
+  ; Check if hl is now zero (annoying that 16 bit DECs don't set status bits)
+  ld a,h
+  or l
+  cp 0
+  jr nz,_decode_all_answer_words
+  ret
+
+; -----------------------------------------------------------------
+; isword - check if 5 letter answer at HL is a valid word
+;          returns A non-zero if word is valid, A = 0 otherwise
+; -----------------------------------------------------------------
+isword:
+  jr _isword_start
+isword_answer_ptr word 0 ; passed value
+isword_buffer     defs 5 ; scratch word
+isword_counter    word 0 ; for answer.txt words
+_isword_start:
+  ld (isword_answer_ptr),hl
+  ld hl,2313 ; word count in answer.txt
+  ld (isword_counter),hl
+  bitstream_reset answer_data
+_search_all_answer_values
+  ; loop through all answers [1,2313] using getpuz.
+  ld ix,isword_buffer
+  call decode_answer_word
+  ; Check if the answer word decoded is the passed word.
+  ld ix,isword_buffer
+  ld iy,(isword_answer_ptr)
+  call equals_check
+  cp 0
+  jr nz,_ret_is_valid
+  ; Decrement our counter (going through all the answers).
+  ld hl,(isword_counter)
+  dec hl
+  ld (isword_counter),hl
+  ; Check if hl is now zero (annoying that 16 bit DECs don't set status bits)
+  ld a,h
+  or l
+  cp 0
+  jr nz,_search_all_answer_values
+_ret_is_not_valid:
+  ld a,0
+  ret
+_ret_is_valid:
+  ld a,1
+  ret
+
+; -- DICTIONARY HELPER FUNCTIONS --
+
+; -----------------------------------------------------------------
+; decode_answer_word - decodes 5 letters into buffer pointed to by ix
+;                      the bitstream is advanced
+; -----------------------------------------------------------------
+decode_answer_word:
   call huffman_decode_char
   ld (ix),a
   call huffman_decode_char
@@ -46,20 +106,33 @@ _decode_answer_word:
   ld (ix+3),a
   call huffman_decode_char
   ld (ix+4),a
-  
-  ld hl,(counter)
-  dec hl
-  ld (counter),hl
-  ; Check if hl is now zero (annoying that 16 bit DECs don't set status bits)
-  ld a,h
-  or l
-  cmp 0
-  jr nz,_decode_answer_word
   ret
 
-; isword - check if 5 letter answer at HL is a valid word
-
-isword:	ret
+; -----------------------------------------------------------------
+; equals_check - checks if 5 letters pointed to by ix and iy are equal.
+;                returns A non-zero if equal, A = 0 otherwise
+; -----------------------------------------------------------------
+equals_check:
+  ld a,(ix)
+  cp (iy)
+  jr nz,_not_equal
+  ld a,(ix+1)
+  cp (iy+1)
+  jr nz,_not_equal
+  ld a,(ix+2)
+  cp (iy+2)
+  jr nz,_not_equal
+  ld a,(ix+3)
+  cp (iy+3)
+  jr nz,_not_equal
+  ld a,(ix+4)
+  cp (iy+4)
+  jr nz,_not_equal
+  ld a,1 ; strings are equal
+  ret
+_not_equal:
+  ld a,0
+  ret
 
 ; The total size of subroutines and data.
 dict_size equ $-dict_start
--- etc/trs8bit2022_contest/tests/dictionary_test.asm
+++ etc/trs8bit2022_contest/tests/dictionary_test.asm
@@ -6,7 +6,54 @@ import '../dictionary.asm'
 
 buffer defs 5
 
+answer1 defb 'TANDY'
+answer2 defb 'RADIO'
+answer3 defb 'SHACK'
+answer4 defb 'CIGAR'
+answer5 defb 'FICUS'
+answer6 defb 'DWARF'
+
+badword1 defb '-----'
+badword2 defb 'FFFFF'
+badword3 defb '     '
+
 main:
+  z80unit_test 'helper check_equals'
+
+  ld ix,answer1
+  ld iy,answer1
+  call equals_check
+  assertEquals8 1,a ; true
+
+  ld ix,answer1
+  ld iy,answer2
+  call equals_check
+  assertEquals8 0,a ; false
+
+  z80unit_test 'helper decode_answer_word'
+
+  bitstream_reset answer_data
+  ld ix,buffer
+  call decode_answer_word
+  ld ix,buffer
+  ld iy,answer1
+  call equals_check
+  assertEquals8 1,a ; true should be 'TANDY'
+
+  ld ix,buffer
+  call decode_answer_word
+  ld ix,buffer
+  ld iy,answer2
+  call equals_check
+  assertEquals8 1,a ; true should be 'RADIO'
+
+  ld ix,buffer
+  call decode_answer_word
+  ld ix,buffer
+  ld iy,answer3
+  call equals_check
+  assertEquals8 1,a ; true should be 'SHACK'
+
   z80unit_test 'getpuz first answers'
 
   ld de,1
@@ -57,6 +104,46 @@ main:
   ld hl,buffer
   call getpuz
   assertMemString buffer,'RUMOR'
+
+  z80unit_test 'isword from answers'
+
+  ld hl,answer1
+  call isword
+  assertEquals8 1,a ; true
+
+  ld hl,answer2
+  call isword
+  assertEquals8 1,a ; true
+
+  ld hl,answer3
+  call isword
+  assertEquals8 1,a ; true
+
+  ld hl,answer4
+  call isword
+  assertEquals8 1,a ; true
+
+  ld hl,answer5
+  call isword
+  assertEquals8 1,a ; true
+
+  ld hl,answer6
+  call isword
+  assertEquals8 1,a ; true
+
+  z80unit_test 'isword not a word'
+
+  ld hl,badword1
+  call isword
+  assertEquals8 0,a ; false
+
+  ld hl,badword2
+  call isword
+  assertEquals8 0,a ; false
+
+  ld hl,badword3
+  call isword
+  assertEquals8 0,a ; false
 
   z80unit_end_and_exit
   end main
